0c8da254d4f2c42affa78a79dbaa6fef24ef043a
Michael McCoy <mmccoy@google.com>
2015-06-15 13:43:23 -0400

    Bucket events by hour

--- app/scripts/mmh.js
+++ app/scripts/mmh.js
@@ -1,4 +1,14 @@
 Historian.setWorkerPath('bower_components/chrome-historian/src/workers/');
+moment.locale('en', {
+    calendar : {
+        lastDay : '[Yesterday]',
+        sameDay : '[Today]',
+        nextDay : '[Tomorrow at]',
+        lastWeek : '[last] dddd [at]',
+        nextWeek : 'dddd [at]',
+        sameElse : 'L'
+    }
+});
 
 var Merged = {};
 var devices = new Historian.Devices();
@@ -9,23 +19,35 @@ var yesterday = moment().subtract(1, 'days').toDate();
 var dayHistory = new Historian.Day(today);
 var yesterdayHistory = new Historian.Day(yesterday);
 
-moment.locale('en', {
-    calendar : {
-        lastDay : '[Yesterday]',
-        sameDay : '[Today]',
-        nextDay : '[Tomorrow at] LT',
-        lastWeek : '[last] dddd [at] LT',
-        nextWeek : 'dddd [at] LT',
-        sameElse : 'L'
+_.groupByMulti = function (obj, values, context) {
+    if (!values.length)
+        return obj;
+    var byFirst = _.groupBy(obj, values[0], context),
+        rest = values.slice(1);
+    for (var prop in byFirst) {
+        byFirst[prop] = _.groupByMulti(byFirst[prop], rest, context);
     }
-});
+    return byFirst;
+};
+
+_.truncateString = function(str, len) {
+  if (str.length > len && str.length > 0) {
+      var new_str = str + " ";
+      new_str = str.substr (0, len);
+      new_str = str.substr (0, new_str.lastIndexOf(" "));
+      new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+
+      return new_str + '...';
+  }
+  return str;
+}
 
 devices.fetch(function(devices){
   if (devices) { Merged.devices = devices; }
 });
 
-chrome.bookmarks.getRecent(100, function(data) {
-  Merged.bookmarks = data;
+chrome.bookmarks.getTree(function(items) {
+  Merged.bookmarkKeys = items;
 });
 
 dayHistory.fetch(function(visits) {
@@ -38,20 +60,23 @@ yesterdayHistory.fetch(function(visits) {
 
 Merged.renderTemplate = function(data) {
   var context = {
-    "days" : data
-  }
-  console.log(context);
+    "days" : data,
+    "devices": Merged.devices
+  };
   var source = $("#historyTemplate").html();
   var template = Handlebars.compile(source);
   $('.stream').append(template(context));
+  console.log(context);
 };
 
 Merged.getMergedStream = function() {
   var merged = _.union(this.todayVisits, this.yesterdayVisits);
-  var merged = _.union(merged, this.bookmarks);
+  // var merged = _.union(merged, this.bookmarks);
 
   for (var i = 0; i < merged.length; i++) {
     var item = merged[i];
+
+    item.downloads = [];
 
     // Bookmark event
     if (item.dateAdded) {
@@ -70,8 +95,12 @@ Merged.getMergedStream = function() {
       item.dateNormalized = moment(item.endTime);
       item.eventType = 'download';
     }
+
     item.timeFromNow = moment(item.dateNormalized).fromNow();
     item.calendarTime = moment(item.dateNormalized).calendar();
+    // item.hour = moment(item.dateNormalized).get('hour');
+    item.hour = moment(item.dateNormalized).format('h A');
+    item.title = _.truncateString(item.title, 100);
   }
 
   var mergedSorted = _.sortBy(merged, function(o) {
@@ -79,12 +108,8 @@ Merged.getMergedStream = function() {
   });
 
   mergedSorted = mergedSorted.reverse();
-
-  var groupedByDay = _.groupBy(mergedSorted, function(item) {
-    // return item.dateNormalized.format('MMMM Do, YYYY');
-    return moment(item.dateNormalized).calendar();
-  });
-  this.renderTemplate(groupedByDay);
+  var grouped = _.groupByMulti(mergedSorted, ['calendarTime', 'hour']);
+  this.renderTemplate(grouped);
 };
 
 Handlebars.registerHelper('dateFormat', function(context, block) {
@@ -104,18 +129,6 @@ Handlebars.registerHelper('ifEqual', function(item, eventType, options) {
     } else {
         return options.fn(this);
     }
-});
-
-Handlebars.registerHelper ('truncate', function (str, len) {
-    if (str.length > len && str.length > 0) {
-        var new_str = str + " ";
-        new_str = str.substr (0, len);
-        new_str = str.substr (0, new_str.lastIndexOf(" "));
-        new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
-
-        return new Handlebars.SafeString ( new_str +'...' );
-    }
-    return str;
 });
 
 $( ".type-filter" ).change(function(e) {
